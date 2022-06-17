88e8bde03d14f9edf46de38dee59b3b9f85ff3de
Dogukansvnc <101670829+Dogukansvnc@users.noreply.github.com>
2022-06-17 16:30:48 +0300

    .

---
+++ System.Service/DtoMapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Core.DTOs;
+using System.Core.Models;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Service
+{
+    internal class DtoMapper : Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<ProductDto, Product>().ReverseMap();
+            CreateMap<UserAppDto, UserApp>().ReverseMap();
+        }
+    }
+}
